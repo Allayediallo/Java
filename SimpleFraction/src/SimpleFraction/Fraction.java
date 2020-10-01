eb61fb6d89f1c6a88c98ffb0100f0772d4d474ee
Allaye DIALLO <allaye94@gmail.com>
2020-10-01 11:26:17 +0200

    commit avant publication des modifications

--- SimpleFraction/src/SimpleFraction/Fraction.java
+++ SimpleFraction/src/SimpleFraction/Fraction.java
@@ -1,5 +1,15 @@
 package SimpleFraction;
-
+import java.lang.String;
 public class Fraction {
-
+	double num;
+	double den;
+	public Fraction(double num, double den)
+	{
+		this.num = num;
+		this.den = den;
+	}
+	public String toString()
+	{
+		return this.num + "/" + this.den;
+	}
 }
--- SimpleFraction/src/SimpleFraction/Main.java
+++ SimpleFraction/src/SimpleFraction/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		Fraction frac = new Fraction(12,5);
+		System.out.println(frac.toString());
 	}
 
 }
